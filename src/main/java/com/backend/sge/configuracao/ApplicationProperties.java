ab29d35b4d9682c4c16a02d1ce232db4ee9736bf
Aluno <aluno@cesed.local>
2024-09-30 18:18:30 -0300

    mensageria

--- src/main/java/com/backend/sge/configuracao/ApplicationProperties.java
+++ src/main/java/com/backend/sge/configuracao/ApplicationProperties.java
@@ -1,4 +1,18 @@
 package com.backend.sge.configuracao;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "application.mail")
+
 public class ApplicationProperties {
+
+    private String enderecoRemetente;
+    private String nomeRemetente;
+
 }
--- src/main/java/com/backend/sge/service/EmailService.java
+++ src/main/java/com/backend/sge/service/EmailService.java
@@ -1,4 +1,36 @@
 package com.backend.sge.service;
 
+import com.backend.sge.domain.dto.EmailDTO;
+import com.backend.sge.service.exceptions.RegraNegocioException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.messaging.MessagingException;
+import org.springframework.stereotype.Service;
+import java.io.UnsupportedEncodingException;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(EmailDTO emailDTO) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            message.setTo(emailDTO.getDestinatario());
+            message.setFrom("sge.basis@gmail.com", "SGE");
+            message.setSubject(emailDTO.getAssunto());
+            for (String s : emailDTO.getCopias()) {
+                message.addCc(s);
+            }
+            message.setText(emailDTO.getCorpo(), true);
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new RegraNegocioException( "error.title");
+        }
+    }
 }
